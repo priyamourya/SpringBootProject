5998dd52f016a72655f3002f15861c94f5dbd285
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2023-02-19 08:17:55 +0530

    Add files via upload
    Added spring boot project

---
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -0,0 +1,34 @@
+package com.springrest.springrest.controller;
+
+import com.springrest.springrest.entities.Course;
+import com.springrest.springrest.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private CourseService courseService;
+//    This is for home page
+    @GetMapping("/home")
+    public String home() {
+        return "This is my home";
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getCourses(){
+        return this.courseService.getCourses();
+    }
+
+    @GetMapping("/courses/{courseId}")
+    public Course getCourse(@PathVariable String courseId){
+        return this.courseService.getCourse(Long.parseLong(courseId));
+    }
+}
---
+++ springrest/src/main/java/com/springrest/springrest/services/courseServiceImpl.java
@@ -0,0 +1,33 @@
+package com.springrest.springrest.services;
+
+import com.springrest.springrest.entities.Course;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class courseServiceImpl implements CourseService {
+    List<Course> list;
+
+    public courseServiceImpl(){
+        list=new ArrayList<>();
+        list.add(new Course(145, "Java Course", "basic Java"));
+        list.add(new Course(146, "Spring boot Course", "basic spring boot"));
+    }
+
+    @Override
+    public List<Course> getCourses() {
+        return list;
+    }
+    @Override
+    public Course getCourse(long courseId) {
+        Course sCourse=null;
+        for(Course course : list){
+            if(course.getId()==courseId){
+                sCourse=course;
+            }
+        }
+        return sCourse;
+    }
+}
